0d5f040fb4311f01b45abf0200787d6fe3132cf7
Joseph <sejokarizz@gmail.com>
2023-04-24 12:14:21 +0300

    function parses a string with format specifiers

---
+++ parser.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * parser - This function parses a string with format specifiers and
+ *          calls the corresponding function for each specifier.
+ *
+ * @format: string to be parsed
+ * @funct_list: array of structs with function pointers and specifiers
+ * @args: va_list of arguments to be used by the functions
+ *
+ * Return: number of characters printed, or -1 if an error occurred
+ */
+
+int parser(const char *format, conver_t funct_list[], va_list args)
+{
+	int i, j, r_val, printed_chars;
+
+	printed_chars = 0;
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			for (j = 0; funct_list[j].sym != NULL; j++)
+			{
+				if (format[i + 1] == funct_list[j].sym[0])
+				{
+					r_val = funct_list[j].f(args);
+					if (r_val == -1)
+						return (-1);
+					printed_chars += r_val;
+					break;
+				}
+			}
+			if (funct_list[j].sym == NULL && format[i + 1] != ' ')
+			{
+				if (format[i + 1] != '\0')
+				{
+					_putchar(format[i]);
+					_putchar(format[i + 1]);
+					printed_chars = printed_chars + 2;
+				}
+				else
+					return (-1);
+			}
+			i = i + 1;
+		}
+		else
+		{
+			_putchar(format[i]);
+			printed_chars++;
+		}
+	}
+	return (printed_chars);
+}
