8df48e154b6e87b1cdfcc17299bccf8b7aac14f8
Joseph <sejokarizz@gmail.com>
2023-04-24 12:13:46 +0300

    create header files with function prototypes

---
+++ main.h
@@ -0,0 +1,43 @@
+#ifndef MY_PRINTF
+#define MY_PRINTF
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdint.h>
+
+
+/**
+ * struct convert - Struct for conversion specifier and corresponding function
+ * @sym: The conversion specifier character
+ * @f: A pointer to a function that takes a va_list and returns an integer
+ */
+struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+};
+typedef struct convert conver_t;
+
+
+int _putchar(char c);
+
+int _printf(const char *format, ...);
+
+void _vprintf(const char *format, va_list args);
+
+int parser(const char *format, conver_t funct_list[], va_list args);
+
+int p_char(va_list);
+
+int p_string(va_list args);
+
+int p_percent(va_list);
+
+int print_unsgined_number(unsigned int n);
+
+int print_number(va_list);
+
+int p_integer(va_list);
+
+#endif
